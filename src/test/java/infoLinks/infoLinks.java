fba5abe7d126464ed244e82e738f73789c95d351
murtazaaylak <nevalugr@gmail.com>
2023-10-09 14:26:59 +0300

    refactor: refactors folder structure

--- src/test/java/infoLinks/infoLinks.java
+++ src/test/java/infoLinks/infoLinks.java
@@ -1,5 +1,6 @@
 package infoLinks;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,8 +14,19 @@ public class infoLinks {
 
 		driver.get("http://www.automationpractice.pl/index.php?");
 		driver.manage().window().maximize();
-		
 
+		// Find Information links
+
+		Integer act_links = driver.findElements(By.className("item")).size();
+		Integer expct_links = 3;
+		
+		//Check the number of the links in the info section
+		
+		if (act_links.equals(expct_links)) {
+			System.out.println("There are " + expct_links + " links in the Information section: AS EXPECTED");
+		} else {
+			System.out.println("There are" + expct_links + " links in the Information section: NOT EXPECTED");
+		}
 	}
 
 }
